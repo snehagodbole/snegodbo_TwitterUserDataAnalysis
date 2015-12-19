42936fa8195821833487b7c47796df8bcd3531de
sneha godbole <sneha.godbole21@gmail.com>
2015-12-18 22:24:33 -0500

    In this project i have used twitter dataset to calculate meaningful insights.
    I have attached all the files related to the project.
    
    Student Name:Sneha Godbole
    Email:snegodbo@umail.com

---
+++ Project_BDAA/Top10Users.js
@@ -0,0 +1,19 @@
+function getTopUsers(){
+	var _temp =[
+							[ 'Top 10 Influential People',
+									'Number Of Followers' ],
+							[ 'rollingstoneBR', 239413 ],
+							[ 'juliaperrez', 172962 ],
+							[ 'RanaDaggubati', 129696 ],
+							[ 'stephenkruiser', 124849 ], [ 'ShesMD', 114765 ],
+							[ 'YesThatsSoTrue', 113550 ],
+							[ 'iBiebsHeart', 105578 ],
+							[ 'iBiebsHeart', 105578 ],
+							[ 'kristenschaaled', 102140 ],
+							[ 'RealTonyRocha', 98965 ]
+
+					];
+					
+					return _temp;
+	
+}
